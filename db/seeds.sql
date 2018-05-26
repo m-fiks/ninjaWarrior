912026f3fae14a10610a53fdc750cb15998f794c
femioye <johnytrip4u@yahoo.com>
2018-05-26 11:44:53 -0500

    commit changes

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO ninjainfo (information, scores) VALUES ('sallah', '32');
+INSERT INTO ninjainfo (information, scores) VALUES (' ', '');
+INSERT INTO ninjainfo (information, scores) VALUES (' ', '');
+INSERT INTO ninjainfo (information, scores) VALUES (' ', '');
+INSERT INTO ninjainfo (information, scores) VALUES (' ', '');
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const path = require("path");
+var bodyParser = require("body-parser");
 
-app = express();
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+var db = require("./models");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //static path
 // app.use(express.static(path.join(__dirname, "/assets")));
+app.use(express.static("public"));
 
 app.get("/", (req,res) => {
     res.send("welcome to the game");
 })
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
+require("./routes/api-routes.js")(app);
 
-// testing branch merges
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
