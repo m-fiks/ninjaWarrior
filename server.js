57a1de7ffad8dc91d0006a66b89fcc88bd2152ce
wdarsow <wade.darsow@gmail.com>
2018-05-25 21:23:35 -0500

    removed one blank line / test repo push

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 8080;
 //static path
 // app.use(express.static(path.join(__dirname, "/assets")));
 
-
 app.get("/", (req,res) => {
     res.send("welcome to the game");
 })
