ddcdee3477a01a6307a0cc7b631e0ecace51ff55
wdarsow <wade.darsow@gmail.com>
2018-05-26 11:06:10 -0500

    additional changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app = express();
 const PORT = process.env.PORT || 8080;
 
 //static path
-// app.use(express.static(path.join(__dirname, "/assets")));
+// app.use(express.static(path.join(__dirname, "/assets")))
 
 app.get("/", (req,res) => {
     res.send("welcome to the game");
