77673c8b69c89d29e73a0e49b070957764a99386
Sama Sandy <samasandy86@gmail.com>
2018-06-02 14:53:25 -0500

    Adding the Game to the Game HTML

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@
 const Sequelize = require('sequelize');
 
 //Creates MySQL connection using Sequelize 
-const sequelize = new Sequelize('ninjawarrior_db', 'root', '', {
+const sequelize = new Sequelize('ninjawarrior_db', 'root', 'Badminton1!', {
     host: 'localhost',
     dialect: 'mysql',
   
