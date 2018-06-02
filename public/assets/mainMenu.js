d1a003cbb773cde57ff5473f9fde32cca9397959
osman144 <osman144@umn.edu>
2018-06-02 02:52:58 -0500

    added main menu and scenes

---
+++ public/assets/mainMenu.js
@@ -0,0 +1,48 @@
+let play;
+class mainMenu extends Phaser.Scene {
+    constructor() {
+        super({key:"mainMenu"});
+    }
+    
+    //load the images
+    preload () {
+    //sky needs to be larger to fit the world bounds
+    this.load.image('sky', './assets/images/sky.png');
+    //Play button
+    this.load.image('play', './assets/images/play.png')
+    //How to play button
+    this.load.image('help', './assets/images/help.png')
+    };
+
+    create (){
+    //adding sky
+    let background = this.add.image(50, 0, 'sky');
+    //Some text
+    let main = this.add.text(225,0, 'Main Menu, Welcome!', {font:"40px Impact", fill: "#000000"});
+    //buttons click events
+
+    play = this.add.text(250, 190, 'Play now!', {font:"20px Impact", fill:'#ffffff'}).setInteractive();
+    
+    this.input.on('pointerdown', function (event) {
+        this.scene.start("Scene1")
+    }, this);
+    // let playButton = this.add.image(250, 260, 'play');
+
+    let how = this.add.text(400, 190, 'How to play', {font:"20px Impact", fill:'#ffffff'});
+    // let helpButton = this.add.image(300, 260, 'help');
+
+
+    };
+
+    // startGame (event) {
+    //     this.scene.start("Scene1");
+    // };
+
+    showHighScores () {
+        this.game.state.start('Checkout high scores here...');
+    };
+
+    showHowToPlay () {
+        this.game.state.start('HowToPlay');
+    }
+};
