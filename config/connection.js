f6402bd70bb217296ff79e82b2704e18a775abce
wdarsow <wade.darsow@gmail.com>
2018-06-04 21:59:54 -0500

    misc change

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 
 //Creates MySQL connection using Sequelize 
-const sequelize = new Sequelize('ninjawarrior_db', 'root','', {
+const sequelize = new Sequelize('ninjawarrior_db', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql',
   
