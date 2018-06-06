a33203d9ec145c7d320d428d7d6229316bb4337e
m-fiks <fiksd005@umn.edu>
2018-06-05 21:19:09 -0500

    game complete

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 
 //Creates MySQL connection using Sequelize 
-const sequelize = new Sequelize('ninjawarrior_db', 'root', 'root', {
+const sequelize = new Sequelize('ninjawarrior_db', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
   
