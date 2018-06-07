17041a6eda7503bf80e9756ca01dfb8e099fc8c4
m-fiks <fiksd005@umn.edu>
2018-06-07 00:32:26 -0500

    make better

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
 // THIS FILE INITIATES THE CONNECTION TO MYSQL
 // Dependencies 
+
 const Sequelize = require('sequelize');
 
-//Creates MySQL connection using Sequelize 
-
-const sequelize = new Sequelize('ninjawarrior_db', 'root', '', {
+  //Creates MySQL connection using Sequelize 
+  const sequelize = new Sequelize('ninjawarrior_db', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-  
-    pool: {
+
+      pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
     },
-});
+  });
+
 
 module.exports = sequelize;
