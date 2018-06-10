c8239d5b9073af08282081672e9d9ebf088c19e7
wdarsow <wade.darsow@gmail.com>
2018-06-10 08:41:10 -0500

    presentation changes

--- public/assets/Score.js
+++ public/assets/Score.js
@@ -9,17 +9,18 @@ class Score extends Phaser.Scene {
     //load the images
     preload () {
     //sky needs to be larger to fit the world bounds
-    this.load.image('ninja', './assets/images/game_background.jpg');
+    this.load.image('ninja', './assets/images/ninja_background.jpg');
     };
 
     create (){
     //adding ninja
-    let background = this.add.image(50, 0, 'ninja');
+    let background = this.add.image(400, 350, 'ninja');
     //Some text
     let main = this.add.text(225,0, 'End!', {font:"40px Impact", fill: "#000000"});
     //buttons click events
 
     let scores = this.add.text(350, 190, 'Score', {font:"25px Impact", fill:'#ffffff'}).setInteractive();
+    let user = this.add.text(250, 190, 'User', {font:"25px Impact", fill:'#ffffff'})
     allScores = this.add.text(350, 220, 'HOWDY',  {font:"25px Impact", fill:'#ffffff'})
     allUsername = this.add.text(250, 220, 'JHJKH',  {font:"25px Impact", fill:'#ffffff'})
     // let data = 
@@ -57,7 +58,7 @@ class Score extends Phaser.Scene {
     }, this);
     // let playButton = this.add.image(250, 260, 'play');
 
-    let how = this.add.text(400, 190, 'How to play', {font:"20px Impact", fill:'#ffffff'});
+    // let how = this.add.text(400, 190, 'How to play', {font:"20px Impact", fill:'#ffffff'});
     // let helpButton = this.add.image(300, 260, 'help');
 
 
